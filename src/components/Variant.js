ab83f84e85c47e5e1e27f0302bd4528c5bb494bf
MacBook <ke6a93@gmail.com>
2017-11-26 21:38:37 +0300

    voting

--- src/components/Variant.js
+++ src/components/Variant.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import checked from '../style/images/checked.svg';
 
 const Variant = (props) => {
     return(
-        <div>
+        <div className="col-sm-6 col-sm-offset-3 variant">
             <h2>{props.name}</h2>
             <h3>{props.description}</h3>
-            <h3>{props.chosen ? 'Выбрано!' : null}</h3>
+            {
+                props.chosen ?
+                    <img width="50px" src={checked} alt="Галочка"/>
+                    : null
+            }
         </div>
     );
 };
--- src/components/Voting.js
+++ src/components/Voting.js
@@ -38,48 +38,59 @@ class Voting extends React.Component {
         if (voting === null) {
             return null;
         }
+        let alive = this.state.alive;
         let openDate = new Date(voting.openDate).toLocaleString('ru');
         return (
-            <div className="container">
-                <div className="row">
-                    <header className="center">
-                        <h1>{voting.name}</h1>
-                        <p>by {voting.creator.login}</p>
-                        <h3>{voting.description}</h3>
-                    </header>
-                    <p>Создано: {openDate}</p>
-                    {
-                        this.state.alive ?
-                            <Timer border={voting.closingDate}
-                                   closeVoting={this.setVotingClosed}/> :
-                            <h3>Голосование окончено!</h3>
-                    }
-                    {
-                        voting.variants.map((variant) => {
-                            return (<Variant key={variant.id}
-                                             name={variant.name}
-                                             chosen={variant.id === this.props.chosenVariantId}
-                                             description={variant.description}/>);
-                        })
-                    }
-                    {
-                        this.state.voted ?
-                            <p>Проголосовано.</p> :
-                            !this.state.alive ?
-                                <p>Закрыто</p> :
+            <div className="voting-root">
+                <div className="container">
+                    <div className="row center content">
+                        <header>
+                            <h1>{voting.name}</h1>
+                            <p>by {voting.creator.login}</p>
+                            <h3>{voting.description}</h3>
+                        </header>
+                        {
+                            alive ?
+                                <Timer border={voting.closingDate}
+                                       closeVoting={this.setVotingClosed}/> :
                                 <div>
-                                    <button onClick={this.handleCreateVote.bind(this, voting.variants[0])}>Вариант1
-                                    </button>
-                                    <button onClick={this.handleCreateVote.bind(this, voting.variants[1])}>Вариант2
-                                    </button>
+                                    <h3 className="over">Голосование окончено!</h3>
+                                    <Link to={`/voting/${voting.id}/result`}>Просмотреть результат</Link>
                                 </div>
-                    }
-                    {
-                        voting.imageLink !== null ?
-                            <img src={voting.imageLink} alt="Голосование"/> : null
-                    }
-                    <Link to={`/voting/${voting.id}/result`}>Результат</Link>
-                    <Link to="/list">Назад</Link>
+                        }
+                        {
+                            voting.imageLink !== null ?
+                                <img src={voting.imageLink} alt="Голосование"/> : null
+                        }
+                        {
+                            alive ?
+                                voting.variants.map((variant) => {
+                                    return (<Variant key={variant.id}
+                                                     name={variant.name}
+                                                     chosen={variant.id === this.props.chosenVariantId}
+                                                     description={variant.description}/>);
+                                }) : null
+                        }
+                        {
+                            this.state.voted ?
+                                <p>Проголосовано.</p> :
+                                alive ?
+                                    <div>
+                                        <button onClick={this.handleCreateVote.bind(this, voting.variants[0])}>
+                                            Вариант1
+                                        </button>
+                                        <button onClick={this.handleCreateVote.bind(this, voting.variants[1])}>
+                                            Вариант2
+                                        </button>
+                                    </div> : null
+                        }
+                        <div className="col-sm-12 mt-30">
+                            <p>Добавлено: {openDate}</p>
+                        </div>
+                        <div className="col-sm-12 mt-30">
+                            <Link className="button" to="/list">Назад</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
